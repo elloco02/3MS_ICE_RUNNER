5aa48d5475c65ac2d8f03137b0910227feecd5ab
s-rehan <sajid.rehanahmad@gmail.com>
2025-01-07 20:36:05 +0100

    feat(coin-system): add coin tracking and display functionality (#37)
    - Renamed collider component to `Coin` in `CoinPrefab`
    - Added new UI elements for displaying the current coin count in the pause menu (`currentCoinCount` GameObject and `coinCount` TextMeshProUGUI).
    - Implemented static `Collectable.Instance` for easy access to the collectibles data across scenes.
    - Integrated coin count saving and loading using `PlayerPrefs` for persistence.
    - Updated `Collectable` class to manage coin collection and update UI when coins are added or removed.
    - Added logic to pause menu to display the current coin count when the game is paused.

--- IceRunner/Assets/Scripts/PauseMenu.cs
+++ IceRunner/Assets/Scripts/PauseMenu.cs
@@ -1,18 +1,27 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class PauseMenu : MonoBehaviour
-{   
+{
     [SerializeField] GameObject pauseMenu;
+    [SerializeField] GameObject currentCoinCount;
+    [SerializeField] public TextMeshProUGUI coinCount;
+
+
     public void Pause()
     {
         pauseMenu.SetActive(true);
+        currentCoinCount.SetActive(true);
         Time.timeScale = 0;
+        UpdateCoinDisplay();
     }
 
     public void Resume()
     {
         pauseMenu.SetActive(false);
+        currentCoinCount.SetActive(false);
         Time.timeScale = 1;
     }
 
@@ -26,5 +35,19 @@ public class PauseMenu : MonoBehaviour
     { 
         SceneManager.LoadScene("Main Menu");
         Time.timeScale = 1;
+    }
+
+    private void UpdateCoinDisplay()
+    {
+        if (Collectable.Instance != null)
+        {
+            int coinCountValue = Collectable.Instance.GetCoinCount();
+            Debug.Log($"Coin count: {coinCountValue}");
+            coinCount.text = $"{coinCountValue}";
+        }
+        else
+        {
+            Debug.LogError("Collectable instance is null!");
+        }
     }
 }
